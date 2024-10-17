001b79c508fd1a312b80bf9c187f0d65bf76934a
KazuyaSuzuki <83203852+ve1997@users.noreply.github.com>
2024-10-17 18:11:40 +0900

    style: アプリのスタイルをインポート

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,4 @@
+import type { LinksFunction } from "@remix-run/node";
 import {
 	Form,
 	Links,
@@ -5,6 +6,11 @@ import {
 	Scripts,
 	ScrollRestoration,
 } from "@remix-run/react";
+import appStyleHref from "./app.css?url";
+
+export const links: LinksFunction = () => [
+	{ rel: "stylesheet", href: appStyleHref },
+];
 
 export default function App() {
 	return (
